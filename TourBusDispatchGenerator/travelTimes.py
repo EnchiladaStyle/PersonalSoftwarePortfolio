857420e1c2849243f98001f2632087a039cbf1f9
EnchiladaStyle <jon19073@byui.edu>
2024-09-12 15:16:39 -0600

    cleaned up some code

--- TourBusDispatchGenerator/travelTimes.py
+++ TourBusDispatchGenerator/travelTimes.py
@@ -1,6 +1,5 @@
 
 taquanTimes = {
-    "Dock 3": dock3Times["Taquan"],
     "Ward Cove": wardCoveTimes["Taquan"],
     "Lumberjack": lumberjackTimes["Taquan"],
     "George Inlet": georgeInletTimes["Taquan"],
