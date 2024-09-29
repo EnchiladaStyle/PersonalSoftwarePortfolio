f855d15a300a3d447c1e0e74bd367f63cbcd2a89
EnchiladaStyle <jon19073@byui.edu>
2024-09-28 20:48:33 -0600

    cleaned up some code

--- TourBusDispatchGenerator/travelTimes.py
+++ TourBusDispatchGenerator/travelTimes.py
@@ -1,7 +1,6 @@
 
 
 jeepBaseTimes = {
-    "Ward Cove": wardCoveTimes["Jeep Base"],
     "Dock 2": dock2Times["Jeep Base"],
     "Dock 1": dock1Times["Jeep Base"],
     "Dock 3": dock3Times["Jeep Base"],
