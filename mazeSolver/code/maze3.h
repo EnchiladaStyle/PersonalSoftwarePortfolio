73cbbb17b887340941d66a0b05601f6fbf7f0ec0
EnchiladaStyle <jon19073@byui.edu>
2024-10-23 13:21:13 -0600

    Moving stuff over the my portfolio page

---
+++ mazeSolver/code/maze3.h
@@ -0,0 +1,17 @@
+#ifndef MAZE3_H
+#define MAZE3_H
+
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <thread>
+#include <future>
+#include <mutex>
+
+namespace maze3{
+    std::vector<std::vector<std::string> > getMaze();
+}
+
+
+#endif
