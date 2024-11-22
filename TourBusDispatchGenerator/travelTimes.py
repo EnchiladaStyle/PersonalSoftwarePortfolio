efa8679d0162526205cad25efbcda03289d1c5b2
EnchiladaStyle <jon19073@byui.edu>
2024-11-22 13:56:48 -0700

    cleaned up some code

--- TourBusDispatchGenerator/travelTimes.py
+++ TourBusDispatchGenerator/travelTimes.py
@@ -1,7 +1,6 @@
 
 
 adventureCartsTimes = {
-    "Taquan": taquanTimes["Adventure Carts"],
     "Jeep Base": jeepBaseTimes["Adventure Carts"],
     "Harriot Hunt": harriotHuntTimes["Adventure Carts"],
     "White River": whiteRiverTimes["Adventure Carts"],
