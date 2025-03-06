3f1161930eb74233d232f6dbbc8d04d35c48ac29
EnchiladaStyle <jon19073@byui.edu>
2025-03-05 20:25:35 -0700

    cleaned up some code

--- DataStructuresTutorial/linkedListPractice.py
+++ DataStructuresTutorial/linkedListPractice.py
@@ -1,10 +1,4 @@
 
-playList = LinkedList()
-
-songs = ["Take On Me", "Sweet Home Alabama", "What Does the Fox Say?", "Heyah", "I'm Blue", "Bad Romance", "Shake It Off", "Moskau"]
-
-for song in songs:
-    playList.addSong(song)
 
 playList.addSong("Take On Me")
 playList.addSong("I'm Blue")
